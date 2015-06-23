ce442ea6e4da779d743c7b6516a6493ae5aa9063
Nami Shah <nami@namis-mbp.home>
2015-06-23 21:47:00 +0200

    Minor fixes

--- src/speech/gui/Constants.java
+++ src/speech/gui/Constants.java
@@ -11,12 +11,12 @@ public interface Constants {
     public final String DRIVER = "com.mysql.jdbc.Driver";
     public final String URL = "jdbc:mysql://localhost/Log";
     public final String USER = "root";
-    public final String PASS = "root";
+    public final String PASS = "";
     public final String PATH_TO_PROFILES = System.getProperty("user.home")+"/Desktop/Speech/profiles";
-    public final int KEY_1 = KeyEvent.VK_SHIFT;
-    public final int KEY_2 = KeyEvent.VK_F3;
+    public final int KEY_1 = KeyEvent.VK_CONTROL;
+    public final int KEY_2 = KeyEvent.VK_1;
     /* ON UPDATE => THIS SHOULD BE THE SAME AS GITHUB RELEASE VERSION */
-    public final String VERSION = "1.0.3";
+    public final String VERSION = "1.0.4";
     public final String VERSION_URL = "https://github.com/ShahNami/Speech/tags";
     public final String DOWNLOAD_FILE_URL = "https://github.com/ShahNami/Speech/releases/download/";
     public final String DOWNLOAD_FILE_NAME = "Speech";
